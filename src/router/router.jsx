424c5a576c3752fe330b687207fe2f810f8eea1b
Rakibislam22 <rakibislam13122018@gmail.com>
2026-05-16 21:16:05 +0600

    feat: add routed pages

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,10 +1,37 @@
 import { createBrowserRouter } from "react-router";
 import App from "../App";
+import SiteLayout from "../layouts/SiteLayout";
+import About from "../pages/About";
+import Collections from "../pages/Collections";
+import Lookbook from "../pages/Lookbook";
+import NewArrivals from "../pages/NewArrivals";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />
+    element: <SiteLayout />,
+    children: [
+      {
+        index: true,
+        element: <App />,
+      },
+      {
+        path: "collections",
+        element: <Collections />,
+      },
+      {
+        path: "new-arrivals",
+        element: <NewArrivals />,
+      },
+      {
+        path: "lookbook",
+        element: <Lookbook />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
   },
 ]);
 
